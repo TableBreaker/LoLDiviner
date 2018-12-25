c5eecd446746c6a341a3296575490b84f9b4dcd4
luyuanpeng <karexy@qq.com>
2018-12-25 15:21:03 +0800

    update

---
+++ Python/Src/data.py
@@ -0,0 +1,13 @@
+class ApiUrl:
+    APIKEY = "RGAPI-7c1b6c40-be55-40ba-b80b-3fb8a2d48cb5"
+    MASTER_LEAGUE_API = "/lol/league/v4/masterleagues/by-queue/"
+    GRANDMASTER_LEAGUE_API = "/lol/league/v4/grandmasterleagues/by-queue/"
+    CHALLENGER_LEAGUE_API = "/lol/league/v4/challengerleagues/by-queue/"
+    MATCH_API = "/lol/match/v4/matches/"
+    ACCOUNT_MATCH_LIST_API = "/lol/match/v4/matchlists/by-account/"
+    SUMMONER_DATA_API = "/lol/summoner/v4/summoners/by-name/"
+
+class ApiParam:
+    SOLORANK = "RANKED_SOLO_5x5"
+    FLEXSR = "RANKED_FLEX_SR"
+    FLEXTT = "RANKED_FLEX_TT"
--- Python/Src/main.py
+++ Python/Src/main.py
@@ -1,45 +1,39 @@
 #coding=utf-8
 import requests
 import json
-
-def requestSummonerData(summonerName, APIKey):
-
-    URL = "https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/" + summonerName + "?api_key=" + APIKey
-    print (URL)
-    response = requests.get(URL)
-    return response.json()
-
-def requestRankedData(ID, APIKey):
-    URL= "https://na1.api.riotgames.com/lol/league/v3/positions/by-summoner/"+ID+"?api_key="+APIKey
-    print (URL)
-    response = requests.get(URL)
-    return response.json()
-
+import sys
+import data
+import time
 
 def main():
+    
+    return 0
 
-    summonerName = (str)(input('Type your Summoner Name here: '))
-    APIKey = (str)(input('Copy and paste your API Key here: '))
-    responseJSON  = requestSummonerData(summonerName, APIKey)
-
-    print(responseJSON)
-    ID = responseJSON ['id']
-    ID = str(ID)
-    print (ID)
-    responseJSON2 = requestRankedData(ID, APIKey)
-    print (responseJSON2[ID][0]['tier'])
-    print (responseJSON2[ID][0]['entries'][0]['rank'])
-    print (responseJSON2[ID][0]['entries'][0]['leaguePoints'])
+def requestSummonerData(summonerName):
+    return None
 
 def requestMasterQueue():
+    return None
+
+def requestGrandMasterQueue():
     return None
 
 def requestChallengerQueue():
     return None
 
-def requestMatchData(summonerName, matchId, APIKey):
+def requestMatchData(matchId):
     return None
 
+def requestMatchList(accountId):
+    return None
+
+def requestURL(API, param):
+    urlFormat = "https://na1.api.riotgames.com{0}{1}?api_key={2}"
+    url = urlFormat.format(API, param, data.ApiUrl.APIKEY)
+    print ("Request: " + url)
+    response = requests.get(url)
+    time.sleep(.05)
+    return response.json()
 
 if __name__ == "__main__":
     main()
